59c5cc022625d57c590d324c5499bc61657b38ac
Alicia Benedetto <aliciabenedetto20@gmail.com>
2026-01-22 21:30:09 -0300

    commit com arquivo da aula de POO, class Animal, Cachorro e Gato

---
+++ teste1.py
@@ -0,0 +1,43 @@
+from abc import abstractmethod
+
+
+class Animal:
+   def __init__ (self, nome, peso, idade, raca, patas):
+       self.nome = nome
+       self.peso = peso
+       self.idade = idade
+       self.raca = raca
+       self.patas = patas
+
+
+       @abstractmethod
+       def saudar(self):
+           pass
+
+
+
+
+class Cachorro(Animal):
+   def __init__(self, nome, peso, idade, raca, patas):
+       super().__init__(nome, peso, idade, raca, patas)
+
+
+   def saudar(self):
+       print('Auau')
+
+
+
+
+class Gato(Animal):
+   def __init__(self, nome, peso, idade, raca, patas):
+       super().__init__(nome, peso, idade, raca, patas)
+   def saudar(self):
+       print('Miau')
+
+
+cachorro1 = Cachorro('Max', 3.2,2,'Doberman', 4)
+gato1 = Gato('Roge', 1.2, 2, 'Pelo curto Brasileiro', 4)
+
+
+cachorro1.saudar()
+gato1.saudar()
